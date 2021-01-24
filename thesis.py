b57dc7440f47bd0fc22619949dd43e3be9686fe0
walter.laurito <walter.laurito@volunteerworld.com>
2021-01-24 16:05:41 +0100

    give aircraft target_heading directions move to target

--- thesis.py
+++ thesis.py
@@ -18,19 +18,27 @@ minutes = 3
 def in_seconds(minutes):
     return minutes * 60
 
+action = np.array([0])
 while time_step <= in_seconds(minutes=2):
     env.render() # comment render() for faster training
-    print("env.action_space.sample()", env.action_space.sample())
-    action = np.array([30])
-    # sample = env.action_space.sample()
-    (pitch_rad, roll_rad, heading_deg, time_step), _, done, _ = env.step(action) # take a random action
+    # print("env.action_space.sample()", env.action_space.sample())
 
-    roll_data.append(np.rad2deg(roll_rad))
+    # sample = env.action_space.sample()
+    state, reward, done, _ = env.step(action) # take a random action
+    aircraft_state, target_information, time_step = state
+
+    action = np.array([target_information["relative_bearing_deg"]])
+
+    print("aircraft_state", aircraft_state)
+    print("target_information", target_information)
+    print("time_step", time_step)
+
+    '''roll_data.append(np.rad2deg(roll_rad))
     pitch_data.append(np.rad2deg(pitch_rad))
     time_steps.append(time_step)
-    heading_data.append(heading_deg)
+    heading_data.append(heading_deg)'''
 
-plotter = PlotterWalt()
-print(time_step)
-plotter.plot(data=[roll_data, pitch_data, heading_data], time=time_steps, titles=["roll", "pitch", "heading"])
-print("done")
+#plotter = PlotterWalt()
+#print(time_step)
+#plotter.plot(data=[roll_data, pitch_data, heading_data], time=time_steps, titles=["roll", "pitch", "heading"])
+#print("done")
