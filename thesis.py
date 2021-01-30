4cccadd7c9bdd6341d312bf424cf51fd1ed587d4
walter.laurito <walter.laurito@volunteerworld.com>
2021-01-30 21:25:28 +0100

    cleanup + add more graphs

--- thesis.py
+++ thesis.py
@@ -5,6 +5,7 @@ import numpy as np
 env = gym.make(id='Guidance-v0',
                jsbsim_path="/Users/walter/thesis_project/jsbsim",
                flightgear_path="/Users/walter/FlightGear.app/Contents/MacOS/")
+
 state = env.reset()
 
 time_steps = []
@@ -22,23 +23,24 @@ def in_seconds(minutes: int) -> int:
 action = np.array([0])
 
 print("start...")
-while time_step <= in_seconds(minutes=1):
+while time_step <= in_seconds(minutes=2):
     # env.render("flightgear") # comment render for faster training
 
     observation = env.step(action)
     print(observation)
     state, reward, done, _ = observation
 
+    target_heading_deg = state["target_heading_deg"]
+    action = np.array([target_heading_deg])
+
+    # diagram stuff
     aircraft_geo_longs.append(state["aircraft_long_deg"])
     aircraft_geo_lats.append(state["aircraft_lat_deg"])
     aircraft_altitudes.append(state["altitude_sl_ft"])
     track_angles.append(state["track_angle_deg"])
     rewards.append(reward)
-
-    target_heading_deg = state["target_heading_deg"]
-    action = np.array([0])
-
     time_steps.append(time_step)
+
     time_step = state["time_step"] + 1
 
 print("done")
@@ -50,6 +52,9 @@ data = [
     aircraft_altitudes,
     time_steps,
     rewards,
-    track_angles
+    track_angles,
+    state["target_lat_deg"],
+    state["target_long_deg"],
+    state["target_altitude_ft"]
 ]
 Map3DPlotter().plot(data)
