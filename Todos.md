fcbec4cb9d327311bc590a4778b668728276a7fe
walter.laurito <walter.laurito@volunteerworld.com>
2021-01-31 15:02:33 +0100

    add cones to show direction of aircraft in diagram

---
+++ Todos.md
@@ -0,0 +1,10 @@
+# Todos
+## Now
+- Simple Training: Train an agent in Google Colab with stable baselines for fixed height (engines on), no wind, no obstacles to get from point A to B.
+
+## Later
+- Add obstacles into state
+- Train considering engines off and altitude
+- Train considering wind, engines off and altitude
+## Questions
+- Consider direction of landing field ?
--- thesis.py
+++ thesis.py
@@ -12,6 +12,11 @@ time_steps = []
 aircraft_geo_lats = []
 aircraft_geo_longs = []
 aircraft_altitudes = []
+
+aircraft_v_easts = []
+aircraft_v_norths = []
+aircraft_v_downs = []
+
 track_angles = []
 rewards = []
 time_step = 0
@@ -37,6 +42,11 @@ while time_step <= in_seconds(minutes=2):
     aircraft_geo_longs.append(state["aircraft_long_deg"])
     aircraft_geo_lats.append(state["aircraft_lat_deg"])
     aircraft_altitudes.append(state["altitude_sl_ft"])
+
+    aircraft_v_easts.append(state["aircraft_v_east_fps"])
+    aircraft_v_norths.append(state["aircraft_v_north_fps"])
+    aircraft_v_downs.append(-state["aircraft_v_down_fps"])
+
     track_angles.append(state["track_angle_deg"])
     rewards.append(reward)
     time_steps.append(time_step)
@@ -50,6 +60,9 @@ data = [
     aircraft_geo_longs,
     aircraft_geo_lats,
     aircraft_altitudes,
+    aircraft_v_downs,
+    aircraft_v_easts,
+    aircraft_v_norths,
     time_steps,
     rewards,
     track_angles,
@@ -57,4 +70,15 @@ data = [
     state["target_long_deg"],
     state["target_altitude_ft"]
 ]
-Map3DPlotter().plot(data)
+Map3DPlotter().plot(long=aircraft_geo_longs,
+                    lat=aircraft_geo_lats,
+                    altitude=aircraft_altitudes,
+                    v_downs=aircraft_v_downs,
+                    v_easts=aircraft_v_easts,
+                    v_norths=aircraft_v_norths,
+                    time=time_steps,
+                    rewards=rewards,
+                    track_angles=track_angles,
+                    target_lat_deg=state["target_lat_deg"],
+                    target_long_deg=state["target_long_deg"],
+                    target_altitude_ft=state["target_altitude_ft"])
