1ec80a77b65e29e07cd52082181be0ccdb8df669
walter.laurito <walter.laurito@volunteerworld.com>
2021-02-02 18:03:48 +0100

    add possibility to render rgb images of diagram for each step to make video later

--- Todos.md
+++ Todos.md
@@ -1,9 +1,10 @@
 # Todos
 ## Now
-- Add big negative reward for out of bounds
-- Create Unit Tests for critical methods before training + manual tests
-- Simple Training: Train an agent in Google Colab with stable baselines for fixed height 
+
+- Check how to render plot as image
+- Simple Training: Train an agent in Google Colab with RLLib for fixed height 
 (engines on), no wind, no obstacles to get from point A to B.
+- Create Unit Tests for critical methods before training + manual tests (While training...)
 
 ## Later
 - Add obstacles into state
@@ -17,4 +18,5 @@
 ## Questions
 - Consider direction of landing field ?
 - Transform from lat / long to x,y ?
-- How to use distances properly?~~~~
+- How to use distances properl instead of lat / long? Does it even matter?
+- Should we give the true heading of the target or really the relative heading of the target to the aircraft
--- thesis.py
+++ thesis.py
@@ -1,16 +1,21 @@
 import gym
 import gym_jsbsim
-from gym_jsbsim.services.plotter import Map3DPlotter
+from gym_jsbsim.services.plotter import MapPlotter
 import numpy as np
-from gym.wrappers import  FlattenObservation
+from gym.wrappers import FlattenObservation
+from matplotlib import pyplot as PLT
+
+
+def in_seconds(minutes: int) -> int:
+    return minutes * 60
 
 env = gym.make(id='Guidance-v0',
                jsbsim_path="/Users/walter/thesis_project/jsbsim",
+               max_episode_time_s=in_seconds(minutes=10),
                flightgear_path="/Users/walter/FlightGear.app/Contents/MacOS/")
 
-state= env.reset()
+state = env.reset() # TODO: get also info on reset?
 print("state", state)
-
 
 time_steps = []
 aircraft_geo_lats = []
@@ -27,19 +32,19 @@ bounds: [gym_jsbsim.utils.GeoPosition] = []
 time_step = 0
 minutes = 3
 
-def in_seconds(minutes: int) -> int:
-    return minutes * 60
 
 action = np.array([0])
-
+info=None
 print("start...")
-while time_step <= in_seconds(minutes=5):
-    # env.render("flightgear") # comment render for faster training
 
-    observation = env.step(action)
-    print("observation", observation)
+done_counter = 0
 
-    state, reward, done, info = observation
+images = []
+while True:
+    images.append(env.render("rgb_array"))
+    state, reward, done, info = env.step(action)
+
+    print("done", done)
 
     target_heading_deg = state["target_heading_deg"]
     action = np.array([target_heading_deg])
@@ -51,7 +56,7 @@ while time_step <= in_seconds(minutes=5):
 
     aircraft_v_easts.append(info["aircraft_v_east_fps"])
     aircraft_v_norths.append(info["aircraft_v_north_fps"])
-    aircraft_v_downs.append( - info["aircraft_v_down_fps"]) # TODO: Sure it is minus here?
+    aircraft_v_downs.append(- info["aircraft_v_down_fps"])  # TODO: Sure it is minus here?
 
     track_angles.append(state["aircraft_track_angle_deg"])
     rewards.append(reward)
@@ -59,26 +64,43 @@ while time_step <= in_seconds(minutes=5):
 
     time_step = info["time_step"] + 1
 
+    if done:
+        done_counter += 1
+        print("done")
+        print("time_step", time_step)
+        break
+
+# show last image with matplot
+# PLT.imshow(images[-1]) # comment render for faster training
+# PLT.show()
+
 print("done")
 print("plot...")
+
+
+
+exit() ######
+
+
+
 
 bound_points = []
 for point in info["bounds"].values():
     lat, long = point.to_array()
     bound_points.append([long, lat])
-bound_points.append(bound_points[0]) # append first point twice for plotting... find a more elegant solution later...
+bound_points.append(bound_points[0])  # append first point twice for plotting... find a more elegant solution later...
 
-Map3DPlotter().plot(long=aircraft_geo_longs,
-                    lat=aircraft_geo_lats,
-                    altitude=aircraft_altitudes,
-                    v_downs=aircraft_v_downs,
-                    v_easts=aircraft_v_easts,
-                    v_norths=aircraft_v_norths,
-                    time=time_steps,
-                    rewards=rewards,
-                    track_angles=track_angles,
-                    target_lat_deg=info["target_lat_deg"],
-                    target_long_deg=info["target_long_deg"],
-                    target_altitude_ft=info["target_altitude_ft"],
-                    bounds=zip(*bound_points))
+MapPlotter().plot(long=aircraft_geo_longs,
+                  lat=aircraft_geo_lats,
+                  altitude=aircraft_altitudes,
+                  v_downs=aircraft_v_downs,
+                  v_easts=aircraft_v_easts,
+                  v_norths=aircraft_v_norths,
+                  time=time_steps,
+                  rewards=rewards,
+                  track_angles=track_angles,
+                  target_lat_deg=info["target_lat_deg"],
+                  target_long_deg=info["target_long_deg"],
+                  target_altitude_ft=info["target_altitude_ft"],
+                  bounds=zip(*bound_points))
 # '''
