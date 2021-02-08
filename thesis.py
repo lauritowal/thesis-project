a6ccb1eafa7472bb1968e4e9e0a4e36ac336728b
walter.laurito <walter.laurito@volunteerworld.com>
2021-02-08 14:34:45 +0100

    fix bounds and target

--- thesis.py
+++ thesis.py
@@ -6,6 +6,7 @@ from gym_jsbsim.services.plotter import MapPlotter
 import numpy as np
 import os
 from gym_jsbsim.utils import in_seconds
+import matplotlib.pyplot as plt
 
 from gym.wrappers import FlattenObservation
 
@@ -56,17 +57,23 @@ env = gym.make(id='guidance-v0',
                flightgear_path="/Users/walter/FlightGear.app/Contents/MacOS/")
 
 env = NormalizeStateEnv(env=env)
+env.reset()
+image = env.render("rgb_array")
+# plt.imshow(image)
+# plt.show()
+
+
+print("env", env)
 
 for episode_counter in range(NUM_EPISODES):
     state = env.reset() # TODO: Common practice to get also info in reset?
     print("state at start", state)
 
-
-    bound_points = []
-    for point in env.get_bounds().values():
-        lat, long = point.to_array()
-        bound_points.append([long, lat])
-    bound_points.append(bound_points[0])  # append first point twice for plotting... find a more elegant solution later...
+    # bound_points = []
+    # for point in env.get_bounds().values():
+    #     lat, long = point.to_array()
+    #     bound_points.append([long, lat])
+    # bound_points.append(bound_points[0])  # append first point twice for plotting... find a more elegant solution later...
 
     sim_time_steps = []
     aircraft_geo_lats = []
@@ -76,10 +83,10 @@ for episode_counter in range(NUM_EPISODES):
     aircraft_v_easts = []
     aircraft_v_norths = []
     aircraft_v_downs = []
-
+    images = []
     track_angles = []
     rewards = []
-    bounds: [gym_jsbsim.utils.GeoPosition] = []
+    # bounds: [gym_jsbsim.utils.GeoPosition] = []
     sim_time_step = 0
     t = 0
 
@@ -88,8 +95,6 @@ for episode_counter in range(NUM_EPISODES):
     print("start...")
 
     done_counter = 0
-
-    images = []
     images.append(env.render("rgb_array"))
 
     while True:
@@ -126,30 +131,30 @@ for episode_counter in range(NUM_EPISODES):
             print("state", state)
             print("###########################")
 
-            # video_file_name = f'{ROOT_DIR}/data/videos/episode_{episode_counter}'
-            # MapPlotter().convert2video(images=images, file_name=video_file_name) # Cleanup...
+            video_file_name = f'{ROOT_DIR}/data/videos/episode_{episode_counter}'
+            MapPlotter.convert2video(images=images, file_name=video_file_name)
 
             # gifs_file_name = f'{ROOT_DIR}/data/gifs/episode_{episode_counter}'
             # MapPlotter().convert2gif(images=images, file_name=gifs_file_name) # Cleanup...
 
-            htmls_file_name = f'{ROOT_DIR}/data/htmls/episode_{episode_counter}'
-            MapPlotter().plot(long=aircraft_geo_longs,
-                              lat=aircraft_geo_lats,
-                              altitude=aircraft_altitudes,
-                              v_downs=aircraft_v_downs,
-                              v_easts=aircraft_v_easts,
-                              v_norths=aircraft_v_norths,
-                              time=sim_time_steps,
-                              rewards=rewards,
-                              track_angles=track_angles,
-                              target_lat_deg=info["target_lat_deg"],
-                              target_long_deg=info["target_long_deg"],
-                              target_altitude_ft=info["target_altitude_ft"],
-                              bounds=zip(*bound_points),
-                              file_name=htmls_file_name,
-                              show=True)
+            # htmls_file_name = f'{ROOT_DIR}/data/htmls/episode_{episode_counter}'
+            # MapPlotter().plot(long=aircraft_geo_longs,
+            #                   lat=aircraft_geo_lats,
+            #                   altitude=aircraft_altitudes,
+            #                   v_downs=aircraft_v_downs,
+            #                   v_easts=aircraft_v_easts,
+            #                   v_norths=aircraft_v_norths,
+            #                   time=sim_time_steps,
+            #                   rewards=rewards,
+            #                   track_angles=track_angles,
+            #                   target_lat_deg=info["target_lat_deg"],
+            #                   target_long_deg=info["target_long_deg"],
+            #                   target_altitude_ft=info["target_altitude_ft"],
+            #                   bounds =zip(*bound_points),
+            #                   file_name=htmls_file_name,
+            #                   show=True)
             break
 
 print("done")
 print("plot...") # for testing
-# '''
+# """
