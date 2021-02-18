f7339c764c0e3a4fdfaec977d454ff7f76d51f9d
walter.laurito <walter.laurito@volunteerworld.com>
2021-02-18 10:12:43 +0100

    add add mean lenght and mean cumulative reward to evalutation + simplify

--- evalutation/evalutation.py
+++ evalutation/evalutation.py
@@ -1,32 +1,39 @@
 import gym
 from gym_jsbsim.agents import RandomAgent
 from gym_jsbsim.normalise_env import NormalizeStateEnv
+import numpy as np
 
+def calculate_baseline_cumulative_reward():
+    env = gym.make(id='guidance-v0',
+                   jsbsim_path="/Users/walter/thesis_project/jsbsim",
+                   max_episode_time_s=60,
+                   flightgear_path="/Users/walter/FlightGear.app/Contents/MacOS/")
 
-class Evalutation():
-    def calculate_baseline_cumulative_reward(self):
-        env = gym.make(id='guidance-v0',
-                       jsbsim_path="/Users/walter/thesis_project/jsbsim",
-                       max_episode_time_s=60,
-                       flightgear_path="/Users/walter/FlightGear.app/Contents/MacOS/")
+    env = NormalizeStateEnv(env=env)
+    agent = RandomAgent(action_space=env.action_space)
 
-        env = NormalizeStateEnv(env=env)
-        agent = RandomAgent(action_space=env.action_space)
-
-        history = []
-        for _ in range(1000):
-            sum_reward = self._run_one_episode(env, agent)
-            history.append(sum_reward)
-        avg_sum_reward = sum(history) / len(history)
-        print("\nbaseline cumulative reward: {:6.2}".format(avg_sum_reward))  # baseline cumulative reward: -1e+03
-
-    def _run_one_episode(self, env, agent):
+    rewards_history = []
+    episode_length_history = []
+    for _ in range(1000):
         env.reset()
         sum_reward = 0
+        t = 0
         while True:
             action = agent.act()
             state, reward, done, info = env.step(action)
             sum_reward += reward
+            t += 1
             if done:
+                rewards_history.append(sum_reward)
+                episode_length_history.append(t)
                 break
-        return sum_reward
+    # print("rewards_history", rewards_history)
+    # print("episode_length_history", episode_length_history)
+    print(f"baseline mean cumulative reward: {np.mean(rewards_history)}")  # baseline cumulative reward: -1e+03
+    print(f"baseline mean episode length: {np.mean(episode_length_history)}")
+
+    return rewards_history, episode_length_history
+
+
+calculate_baseline_cumulative_reward()
+
