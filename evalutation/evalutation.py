ca72a250b662b182d03f471e3367284238f9299c
walter.laurito <walter.laurito@volunteerworld.com>
2021-02-18 11:45:32 +0100

    possibility to set max_distance on env start

--- evalutation/evalutation.py
+++ evalutation/evalutation.py
@@ -6,7 +6,9 @@ import numpy as np
 def calculate_baseline_cumulative_reward():
     env = gym.make(id='guidance-v0',
                    jsbsim_path="/Users/walter/thesis_project/jsbsim",
-                   max_episode_time_s=60,
+                   max_episode_time_s=60*5,
+                   max_distance_km=4,
+                   max_target_distance_km=2,
                    flightgear_path="/Users/walter/FlightGear.app/Contents/MacOS/")
 
     env = NormalizeStateEnv(env=env)
